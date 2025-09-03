2bdc51f22eccf239f7154c19c305984382b0f24b
Truong Nguyen <vonguyen.0407@gmail.com>
2025-09-03 21:16:13 +0700

    deploy 7 transfer to master's branch

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,9 @@ export const metadata = {
     'fc:frame': JSON.stringify({
       type: 'frame',
       name: 'My Mini App',
-      url: 'https://nguyen-miniapp-git-day2-nguyens-projects-a2abcaca.vercel.app',
-      icon: 'https://nguyen-miniapp-git-day2-nguyens-projects-a2abcaca.vercel.app/icon.png',
-      splash: 'https://nguyen-miniapp-git-day2-nguyens-projects-a2abcaca.vercel.app/splash.png',
+      url: 'https://nguyen-miniapp.vercel.app/',
+      icon: 'https://nguyen-miniapp.vercel.app/icon.png',
+      splash: 'https://nguyen-miniapp.vercel.app/splash.png',
       description: 'Short description used for previews',
     }),
   },
